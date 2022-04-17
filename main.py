c16e9911b4a942f3c9cf72f71d5c77196afa271d
Haojie Yu <hyu426@gatech.edu>
2022-04-17 10:05:54 -0500

    data type lessons

--- main.py
+++ main.py
@@ -3,52 +3,46 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
-def draw_giraffe():
-    print("""\
-
-                                           ._ o o
-                                           \_`-)|_
-                                        ,""       \ 
-                                      ,"  ## |   ಠ ಠ. 
-                                    ," ##   ,-\__    `.
-                                  ,"       /     `--._;)
-                                ,"     ## /
-                              ,"   ##    /
+# text type*
+def test_text_type():
+    text = "Good afternoon"
+    print(type(text))
 
 
-                        """)
+# test numeric type()
+def test_numeric_type():
+    a = 4
+    b = 5.2
+    c = 1 + 2j
+    print(type(a))
+    print(type(b))
+    print(type(c))
 
 
-def draw_cat(name):
-    print('                                   ')
-    print('                                /- )')
-    print('                           (  (')
-    print('           A.-.A  .-""-.    )  )')
-    print('         /,  ,\ \/       \ \/  /')
-    print('        =\ \  t   ;=    /      /')
-    print('           `--,\`  .""|      /')
-    print('             || |   \ \ \ \   ')
-    print('              ((,_|    ((,_\ ')
-    print('-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^')
-    print('  This is a lovely cat drew by', name)
+# test bool type
+def test_bool_type():
+    isvalid = True;
+    notvalid = False;
+    print(type(isvalid))
+    print(type(notvalid))
 
 
-def calculate_add():
-    a = 6;
-    b = 9;
-    return a + b
+# test sequence type
+def test_sequence_type():
+    test_list = ["apple", "banana", "cherry"]
+    test_tuple = ("apple", "banana", "cherry")
+    test_range = range(6)
+    for i in range(6):
+        print(i)
+    print(test_list)
+    print(test_tuple)
+    print(test_range)
 
 
-def draw_heart(name):
-    print('      ♥LOVE♥ ' + str(name) + ' ♥LOVE♥           ')
-    print('     ♥LOVE♥♥LOVE♥  ♥LOVE♥♥LOVE♥       ')
-    print('   ♥LOVE♥♥LOVE♥♥LOVE♥♥LOVE♥♥LOVE♥    ')
-    print('      ♥LOVE♥♥LOVE♥♥LOVE♥♥LOVE♥       ')
-    print('         ♥LOVE♥♥LOVE♥♥LOVE♥          ')
-    print('            ♥LOVE♥♥LOVE♥              ')
-    print('              ♥LOVE♥                  ')
-    print('                 ♥                    ')
+# test dictionary type
+def test_dict_type():
+    map = {"name": "John", "age": 36, "color": "red"}
+    print(map["age"])
 
 
 def divide_by_zero():
@@ -59,18 +53,19 @@ def divide_by_zero():
     except ZeroDivisionError:
         print("The divisor cannot be zero")
 
+
 def fibonacci_number(number):
     counter = 0;
     n1 = 0;
     n2 = 1;
     while counter < number:
-        print("The " + str(counter+1) + "th term is " + str(n1))
-        sumNumber = n1 + n2;
-        n1 = n2;
-        n2 = sumNumber;
-        counter = counter + 1;
+        print("The " + str(counter + 1) + "th term is " + str(n1))
+        sumNumber = n1 + n2
+        n1 = n2
+        n2 = sumNumber
+        counter = counter + 1
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    fibonacci_number(6)
+    test_dict_type()
