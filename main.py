5298ebed7439d6a33555f35509bc34a76a02144e
Haojie Yu <hyu426@gatech.edu>
2022-07-16 10:53:00 -0500

    we are learning loop in python today

--- main.py
+++ main.py
@@ -3,64 +3,48 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+def simpleforloop():
+    primes = [2, 3, 5, 7]
+    for prime in primes:
+        print(prime)
 
-def draw_giraffe():
-    print("""\
+def loopusingrange():
+    for x in range(5):
+        print(x)
 
-                                           ._ o o
-                                           \_`-)|_
-                                        ,""       \ 
-                                      ,"  ## |   ಠ ಠ. 
-                                    ," ##   ,-\__    `.
-                                  ,"       /     `--._;)
-                                ,"     ## /
-                              ,"   ##    /
+def loopusingrange2():
+    for x in range(3, 6):
+        print(x)
 
+def loopusingrangeskipcount():
+    for x in range(3, 20, 3):
+        print(x)
 
-                        """)
+def whileloop():
+    count = 0
+    while count < 5:
+        print(count)
+        count += 1
 
+def whileloop2():
+    for x in range(10):
+        # Check if x is even
+        if x % 2 == 0:
+          print(x)
 
-def draw_cat(name):
-    print('                                   ')
-    print('                                /- )')
-    print('                           (  (')
-    print('           A.-.A  .-""-.    )  )')
-    print('         /,  ,\ \/       \ \/  /')
-    print('        =\ \  t   ;=    /      /')
-    print('           `--,\`  .""|      /')
-    print('             || |   \ \ \ \   ')
-    print('              ((,_|    ((,_\ ')
-    print('-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^')
-    print('  This is a lovely cat drew by', name)
+def infiniteloop():
+    count=0
+    while True:
+        print(count)
+        count +=1
 
-
-def calculate_add():
-    a = 6;
-    b = 9;
-    return a + b
-
-
-def draw_heart(name):
-    print('      ♥LOVE♥ ' + str(name) + ' ♥LOVE♥           ')
-    print('     ♥LOVE♥♥LOVE♥  ♥LOVE♥♥LOVE♥       ')
-    print('   ♥LOVE♥♥LOVE♥♥LOVE♥♥LOVE♥♥LOVE♥    ')
-    print('      ♥LOVE♥♥LOVE♥♥LOVE♥♥LOVE♥       ')
-    print('         ♥LOVE♥♥LOVE♥♥LOVE♥          ')
-    print('            ♥LOVE♥♥LOVE♥              ')
-    print('              ♥LOVE♥                  ')
-    print('                 ♥                    ')
-
-
-def divide_by_zero():
-    number1 = 5
-    number2 = 0
-    try:
-        return number1 / number2
-    except ZeroDivisionError:
-        print("The divisor cannot be zero")
-
-
-# Press the green button in the gutter to run the script.
+def whileloopwithbreak():
+    count = 0
+    while True:
+        print(count)
+        count += 1
+        if count >= 5:
+            break
+    # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    #draw_heart('Winston')
-    divide_by_zero()
+    whileloopwithbreak()
