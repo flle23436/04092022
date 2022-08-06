dae13da728ef16028e73a9cad0a2cd6860746b48
Haojie Yu <hyu426@gatech.edu>
2022-08-06 00:29:07 -0500

    We will learning selection on August 6.

--- main.py
+++ main.py
@@ -1,50 +1,54 @@
 # This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def printoddorevent(number):
+    if number % 2 == 0:
+        print("n is even")
+    else:
+        print("n is odd")
 
-def simpleforloop():
-    composites = [4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20]
-    for composite in composites:
-        print(composite)
+def comparenumber(number1, number2):
+    if number1 == number2:
+        print("Both are Equal")
+    elif number1 > number2:
+        print("number1 is greater than number2")
+    else:
+        print("number1 is smaller than number2")
 
-def loopusingrange():
-    for x in range(5):
-        print(x)
+def whatismygrade(grade):
+    if grade >= 80:
+        print("Congratulations, you have earned an A")
+    elif grade >= 65:
+        print("Good job, you have earned a B. Keep going")
+    elif grade >= 50:
+        print("You have earned a C. There is much room to improve.")
+    else:
+       print("Sorry. You failed")
 
-def loopusingrange2():
-    for x in range(4, 20):
-        print(x)
+def numbers_to_strings(argument):
+        switcher = {
+            0: "zero",
+            1: "one",
+            2: "two",
+        }
 
-def loopusingrangeskipcount():
-    for x in range(3, 20, 3):
-        print(x)
+        # get() method of dictionary data type returns
+        # value of passed argument if it is present
+        # in dictionary otherwise second argument will
+        # be assigned as default value of passed argument
+        return switcher.get(argument, "nothing")
 
-def whileloop():
-    count = 0
-    while count <= 5:
-        print(count)
-        count += 1
+def whatistheday(day):
+   match day:
+      case 1:
+         return "It is Monday"
+      case 2:
+          return "It is Tuesday"
+      case 3:
+          return "It is Wednesday"
 
-def whileloop2():
-    for x in range(30):
-        # Check if x is divisible by 5
-        if x % 5 == 0:
-          print(x)
 
-def infiniteloop():
-    count=0
-    while True:
-        print(count)
-        count +=1
 
-def whileloopwithbreak():
-    count = 0
-    while True:
-        print(count)
-        count += 1
-        if count >= 5:
-            break
+
     # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-   simpleforloop()
+   print(whatistheday(0))
